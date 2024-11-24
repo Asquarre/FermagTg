0ee15a26eae3977f98ac24abce074b042c8ee5e2
Asker <askerdursunovof@gmail.com>
2024-11-23 20:13:37 -0700

    Update API URL

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ const App = () => {
 
   const handleOrderSubmit = (orderDetails) => {
     axios
-      .post('http://localhost:3000/submit-order', {
+      .post('https://fermag-tg.vercel.app/submit-order', {
         address: orderDetails.address,
         phone: orderDetails.phone,
         items: cart,
