66f85464ebbee1783fe125e1370f9485877ed259
Asker <askerdursunovof@gmail.com>
2025-06-30 17:23:34 +0500

    interface language fix

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Categories = ({ categories, onSelectCategory }) => {
   return (
     <div>
-      <h2>Categories</h2>
+      <h2>Категории</h2>
       <ul
         style={{
           listStyleType: 'none',
