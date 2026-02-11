86cd6e59148cbf4c871bd939c7683d3ecda649ac
Asker <askerdursunovof@gmail.com>
2026-02-11 19:51:42 +0500

    background update

--- src/components/AnimatedNumber.js
+++ src/components/AnimatedNumber.js
@@ -1,46 +1,32 @@
-import React, { useEffect, useMemo, useRef } from 'react';
-import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import React, { useRef, useEffect } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const AnimatedNumber = ({ value, className = '' }) => {
-  const previousValueRef = useRef(value);
-  const nodeRefs = useRef(new Map());
-
-  const stringValue = useMemo(() => value.toString(), [value]);
-  const direction = Number(value) >= Number(previousValueRef.current) ? 'up' : 'down';
+  const previous = useRef(value);
+  const direction = Number(value) >= Number(previous.current) ? 'up' : 'down';
 
   useEffect(() => {
-    previousValueRef.current = value;
+    previous.current = value;
   }, [value]);
-  
-const getNodeRef = (digitKey) => {
-    if (!nodeRefs.current.has(digitKey)) {
-      nodeRefs.current.set(digitKey, React.createRef());
-    }
-    return nodeRefs.current.get(digitKey);
-  };
+
   return (
     <span className={`animated-number ${className}`}>
-     {stringValue.split('').map((char, index) => {
-        const digitKey = `${index}-${char}`;
-        const nodeRef = getNodeRef(digitKey);
-
-        return (
-          <span key={`slot-${index}`} className="digit-wrapper">
-            <SwitchTransition mode="out-in">
+      {value
+        .toString()
+        .split('')
+        .map((char, index) => (
+          <span key={index} className="digit-wrapper">
+            <TransitionGroup component={null}>
               <CSSTransition
-                key={digitKey}
-                nodeRef={nodeRef}
-                timeout={300}
+                key={`${char}-${index}`}
+                timeout={150}
                 classNames={`digit-${direction}`}
               >
-                <span ref={nodeRef} className="digit">
-                  {char}
-                </span>
+                <span className="digit">{char}</span>
               </CSSTransition>
-            </SwitchTransition>
+            </TransitionGroup>
           </span>
-        );
-      })}
+        ))}
     </span>
   );
 };
